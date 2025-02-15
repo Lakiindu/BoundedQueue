c1827334bb7afda90a85509c8713f70bd9b4ac48
Lakiindu <ransikalakindu@gmail.com>
2025-02-15 15:13:38 +0530

    QueueApp.java

--- QueueApp.java
+++ QueueApp.java
@@ -1,20 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package boundedQueue;
 
-/**
- *
- * @author Vihandu
- */
-public class QueueApp {
-
-    /**
-     * @param args the command line arguments
-     */
+public class Main {
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Create a queue with a max size of 5
+        QueueX queue = new QueueX(5);
+
+        // Enqueue some elements
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.enqueue(40);
+        queue.enqueue(50);
+
+        // Attempting to enqueue when the queue is full
+        queue.enqueue(60);  // This should print "Queue is full"
+
+        // Peek the front element
+        System.out.println("Front element: " + queue.peek());  // Expected: 10
+
+        // Dequeue some elements
+        System.out.println("Dequeue: " + queue.dequeue());  // Expected: 10
+        System.out.println("Dequeue: " + queue.dequeue());  // Expected: 20
+
+        // Peek after dequeuing
+        System.out.println("Front element after dequeue: " + queue.peek());  // Expected: 30
+
+        // Enqueue more elements
+        queue.enqueue(60);
+        queue.enqueue(70);
+
+        // Print all remaining elements in the queue
+        System.out.println("Dequeue: " + queue.dequeue());  // Expected: 30
+        System.out.println("Dequeue: " + queue.dequeue());  // Expected: 40
+        System.out.println("Dequeue: " + queue.dequeue());  // Expected: 50
+        System.out.println("Dequeue: " + queue.dequeue());  // Expected: 60
+        System.out.println("Dequeue: " + queue.dequeue());  // Expected: 70
     }
-    
 }
